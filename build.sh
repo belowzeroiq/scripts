95548ff00a46efa7aaacef24405b9c027960fff9
ordinary <162432464+belowzeroiq@users.noreply.github.com>
2024-03-11 20:34:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,28 @@
-#!/bin/bash
+rm -rf .repo/local_manifests
 
-set -e
-#Credit to Meghthedev for the initial script 
+# Do repo init for rom that we want to build.
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 
-# init
-repo init --depth 1 -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+# Do remove here before repo sync.
+rm -rf hardware
+rm -rf vendor
+rm -rf system
+rm -rf kernel
+rm -rf device
+rm -rf packages
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf out/host
 
-# Run inside foss.crave.io devspace
-# Remove existing local_manifests
-crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
+# Clone our local manifest.
+git clone https://github.com/belowzeroiq/local_manifest --depth 1 -b fourteen/fog .repo/local_manifests
 
-# sync repo
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+# Let's sync!
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+    
+# Define timezone
+export TZ=Asia/Jakarta
 
-# sync tree
-git clone https://github.com/belowzeroiq/local_manifest.git .repo/local_manifests && \
-
-# Set up build environment
-source build/envsetup.sh && \
-
-# Lunch configuration
-lunch lineage_fog-userdebug && \
-
-# Build the ROM
-m bacon"
-
-# Pull generated zip files
-crave pull out/target/product/*/*.zip
+# Let's start build!
+. build/envsetup.sh
+lunch lineage_fog-userdebug
+m bacon
