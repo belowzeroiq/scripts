edb28cc484d71bbf9f02cb1084119a850340cf28
ordinary <162432464+belowzeroiq@users.noreply.github.com>
2024-03-11 20:20:52 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,32 @@
+#!/bin/bash
+
+set -e
+#Credit to Meghthedev for the initial script 
+
+# init
 repo init --depth 1 -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 
+# Run inside foss.crave.io devspace
+# Remove existing local_manifests
 crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
 
-git clone https://github.com/belowzeroiq/local_manifest .repo/local_manifests && \
+# sync repo
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 && \
+# sync tree
+git clone https://github.com/belowzeroiq/local_manifest.git .repo/local_manifests && \
 
-git clone -b fourteen/fog https://github.com/belowzeroiq/device_xiaomi_fog device/xiaomi/fog && \
-
-git clone -b yoru-stock https://github.com/belowzeroiq/artifact-kernel kernel/xiaomi/fog && \
-
-git clone -b fourteen/fog https://github.com/belowzeroiq/vendor_xiaomi_fog vendor/xiaomi/fog && \
-
+# clone hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi && \
 
+# Set up build environment
 source build/envsetup.sh && \
 
+# Lunch configuration
 lunch lineage_fog-userdebug && \
 
-mka bacon”
+# Build the ROM
+m bacon"
 
+# Pull generated zip files
 crave pull out/target/product/*/*.zip
